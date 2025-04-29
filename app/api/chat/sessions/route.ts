e3a2fffbb766ea9306726894f3f1e41a5d961e41
Abdulla <U22103623@sharjah.ac.ae>
2025-04-29 13:57:18 +0400

    Last_Deploy

--- app/api/chat/sessions/route.ts
+++ app/api/chat/sessions/route.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client";
 import { auth } from "@clerk/nextjs/server";
 
 const prisma = new PrismaClient();
+export const dynamic = 'force-dynamic';
 
 export async function GET(req: NextRequest) {
 	try {
