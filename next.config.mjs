878e41ef02321018fe81ed153eb7ec20e543d907
Abdulla <U22103623@sharjah.ac.ae>
2025-04-29 04:12:34 +0400

    testingagainagainagain

--- next.config.mjs
+++ next.config.mjs
@@ -11,11 +11,6 @@ const nextConfig = {
 	},
 };
 
-module.exports = {
-	output: "standalone",  // Required for Azure
-	server: {
-	  port: 8080,  // Explicit port setting
-	},
-  };
+
 
 export default nextConfig;
